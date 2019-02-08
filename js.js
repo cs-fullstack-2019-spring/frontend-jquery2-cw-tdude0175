a4ffed6729f9627b5f0e9288b9bc23b38f44b2c6
Kenn-CodeCrew <40698252+Kenn-CodeCrew@users.noreply.github.com>
2019-02-08 10:35:50 -0600

    Update

--- js.js
+++ js.js
@@ -3,27 +3,10 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 */
 
 
-/* Give the <body> element a background of #bdc3c7 using attr*/
-
-
-/* When you move over the "Selectors Exercise" element, change the height/width to 20px. When you move off of it change it back */
-
-
-/* Make the <h1> element #9b59b6*/
-
-
-/* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
-
-
-/* Make all <li> elements blue(pick your own hexadecimal blue)*/
-
-
-/*Make all inputs have a 3px red border*/
-
-
 /* Give everything with the class 'hello' a white background when you click on one of them*/
 
 
-/* Give the element with id 'special' a 2px solid blue border */
+/* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it change it back */
 
 
+/* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
